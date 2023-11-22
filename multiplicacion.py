b173c7be260498eda19db081e736ce871a52e041
CBarenca <cristian.barenca1897@alumnos.udg.mx>
2023-11-21 20:22:32 -0600

    Commit inicial donde agrego los archivos multiplicacion y webscraping

---
+++ multiplicacion.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Nov 21 18:24:05 2023
+
+@author: ulise
+"""
+
+def multiplica(a,b): 
+    variable1=a*9
+    variable2=b*3
+    resultado=variable1*variable2
+    print("Empezando for")
+    for i in range(1, 5): 
+        print(i)
+        resultado=resultado * i
+        
+        print(resultado)
+    
+multiplica(3,6)
